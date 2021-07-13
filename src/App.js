14886d35dbdfd891365d5053c4c6b3f399330e14
anshul.vandra <anshul.vandra@agileinfoways.com>
2021-07-13 15:03:59 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/Header'
 import Pin from './components/Pin'
 import District from './components/District'
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
